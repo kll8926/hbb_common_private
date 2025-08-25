c870d73ce1203c6e054ab4a8fe0905d14d516e5d
kll8926 <1553790782@qq.com>
2025-08-25 08:47:23 +0800

    修改配置

--- src/config.rs
+++ src/config.rs
@@ -62,13 +62,7 @@ lazy_static::lazy_static! {
     static ref KEY_PAIR: Mutex<Option<KeyPair>> = Default::default();
     static ref USER_DEFAULT_CONFIG: RwLock<(UserDefaultConfig, Instant)> = RwLock::new((UserDefaultConfig::load(), Instant::now()));
     pub static ref NEW_STORED_PEER_CONFIG: Mutex<HashSet<String>> = Default::default();
-    pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>> = {
-        let mut map = HashMap::new();
-        map.insert("verification-method".to_string(), "use-both-passwords".to_string());
-        map.insert("preset-user-name".to_string(), "xyk".to_string());
-        map.insert("preset-device-group-name".to_string(), "新益康".to_string());
-        RwLock::new(map)
-    };
+    pub static ref DEFAULT_SETTINGS: RwLock<HashMap<String, String>>  = Default::default();
     pub static ref OVERWRITE_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref DEFAULT_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref OVERWRITE_DISPLAY_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
